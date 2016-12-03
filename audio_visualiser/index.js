8b59585498bf32dc13f9f1df374a20c47cd4100f
Sam Helas <Samule.Helas.2013@live.rhul.ac.uk>
2016-12-03 18:02:15 +0000

    Amplitude visualisation
    Added a couple of example files in for local reference.
    Imported the p5.sound.js script.
    Preloads the sound, plays it and displays a moving crosshair that
    changes with the amplitude.
    Has to be run from a server.

--- audio_visualiser/index.js
+++ audio_visualiser/index.js
@@ -1,12 +1,34 @@
+var song;
+
+function preload() {
+  song = loadSound('/assets/OGreenWorld.mp3');
+  //song = loadSound('/assets/FeelGoodInc.mp3');
+}
+
 function setup() {
-    createCanvas(640, 480);
+    createCanvas(1080, 720);
+    song.loop();
+
+    // create a new Amplitude analyzer
+    analyzer = new p5.Amplitude();
+
+    // Patch the input to an volume analyzer
+    analyzer.setInput(song);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+  background(255);
+
+  // Get the average (root mean square) amplitude
+  var rms = analyzer.getLevel();
+  fill(127);
+  stroke(0);
+
+  // Draw an ellipse with size based on volume
+  //ellipse(width/2, height/2, 20+rms*200, 20+rms*200);
+
+  rect((width/2)+(rms*200), height/2, rms*200, 2);
+  rect((width/2)-2*(rms*200), height/2, rms*200, 2);
+  rect(width/2, (height/2)+(rms*200), 2, rms*200);
+  rect(width/2, (height/2)-2*(rms*200), 2, rms*200);
 }
